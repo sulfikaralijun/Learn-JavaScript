3a7072b5e6c2c513a6df98b891c76542a1dd41d1
Sulfikar Alijun <116356678+sulfikaralijun@users.noreply.github.com>
2024-01-12 21:39:17 +0800

    update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const voucher = prompt("Masukkan kode diskon Jika Ada: ")
 let total = uang
 if (voucher === 'DISKON50') {
   total -= ((total * 50) / 100)
+} else if (voucher === 'DISKON99') {
+  total -= ((total * 99) / 100)
 }
 
 console.log(total)
