817f4bd238981eb67c63ed45ff5c43ba6a618a7a
Sulfikar Alijun <116356678+sulfikaralijun@users.noreply.github.com>
2024-01-12 21:41:41 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // mengambil jumlah uang kepada user
 const uang = Number(prompt("Masukkan Jumlah Uang: "))
 const voucher = prompt("Masukkan kode diskon Jika Ada: ")
-let total = uang
+let total = 0
 if (voucher === 'DISKON50') {
-  total -= ((total * 50) / 100)
+  total = uang - ((uang * 50) / 100)
 } else if (voucher === 'DISKON99') {
-  total -= ((total * 99) / 100)
+  total = uang - ((uang * 99) / 100)
+} else {
+  total += uang
 }
 
 console.log(total)
