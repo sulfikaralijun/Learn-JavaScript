4bceb1dac6f13a49f474ff0ead7225ff157b5ecf
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-22 20:57:20 +0800

    update script.js

--- src/project/kalkulator/script.js
+++ src/project/kalkulator/script.js
@@ -3,5 +3,5 @@ const input2 = document.getElementById("num2");
 const result = document.getElementById("result");
 
 function plus() {
-  
+  result.innerHTML = Number(input1.value) + Number(input2.value);
 }
